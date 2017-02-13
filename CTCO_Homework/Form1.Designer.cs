a4f76b4b296195a247c4b6911e784fa59f1a9469
Gatis <Gatis@GATIS-PC>
2017-02-13 02:28:58 +0200

    Add project files.

---
+++ CTCO_Homework/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace CTCO_Homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(269, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Service";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(266, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cost($)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 101);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(346, 115);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Services";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 270);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(311, 150);
+            this.dataGridView2.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(149, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Each person\'s total";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(396, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(396, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 25);
+            this.label7.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(474, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "CALCULATE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(401, 101);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 15;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView3.Location = new System.Drawing.Point(364, 169);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(326, 251);
+            this.dataGridView3.TabIndex = 16;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "From";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "To";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Amount($)";
+            this.Column3.Name = "Column3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 432);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ CTCO_Homework/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CTCO_Homework
+{
+    public partial class Form1 : Form
+    {
+        public class Payment
+        {
+            public string Name { get; set; }
+            public string Service { get; set; }
+            public int Price { get; set; }
+        }
+
+        public class PersonTotal
+        {
+            public string Name { get; set; }
+            public int Cost { get; set; }
+        }
+
+
+        List<Payment> Pay = new List<Payment>();
+        List<PersonTotal> Tot = new List<PersonTotal>();
+
+        int total = 0;
+        int average = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Person count
+            int skaititajs = 0;
+            for (int i = 0; i < Tot.Count; i++)
+            {
+                if (textBox1.Text == Tot[i].Name)
+                {
+                    Tot[i].Cost += Convert.ToInt32(textBox3.Text);
+                    skaititajs++;
+                }
+            }
+            if (skaititajs == 0)
+                Tot.Add(new PersonTotal { Name = textBox1.Text, Cost = Convert.ToInt32(textBox3.Text) });
+                Pay.Add(new Payment { Name = textBox1.Text, Service = textBox2.Text, Price = Convert.ToInt32(textBox3.Text) });
+            //videjais average
+            total += Convert.ToInt32(textBox3.Text);
+            label6.Text = "TOTAL($): " + Convert.ToString(total);
+            average = (total / Tot.Count);
+            label7.Text = "AVERAGE($): " + Convert.ToString(average) + " per person";
+
+
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = Pay;
+            dataGridView2.DataSource = null;
+            dataGridView2.DataSource = Tot;
+        }
+        static void transactions(int[] bilance)
+        {
+            int nmax = bilance.Length; // masīva garums
+            int n, i, m;
+            for (n = 0; n < nmax; n++) // cikls pāru meklēšanai pēc masīva elementu skaita (katrā solī nonullējas viena vai divas vērtības ) 
+                                       // ja viss sadalīts visi masīva elementi ir palikuši pa nullēm un cikls pārtrauksies ar break
+            {
+                int minval = 0, minabsval = 0, minvalid = -1;
+                for (i = 0; i < nmax; i++)  // meklē pa masīvu minimālo absolūto vērtību ,kura garantēti dzēsīsies ar pretējās zīmes summu , nulles masīvā ignorē
+                {
+                    m = bilance[i];
+                    if (m != 0) // nulles masīvā ignorē
+                    {
+                        if (m < 0)
+                        {
+                            m = -m; // nomaina zīmi uz plus
+                        }
+                        if (m < minabsval | minvalid < 0)   //  pirmo atrasto nenulles vērtību arī uzskata par minabsval un meklē tālāk
+                        {
+                            minabsval = m;
+                            minvalid = i;  // ja masīvā ir tikai nulles vertība paliek minapsvalid = -1
+                        }
+                    }
+                }
+                if (minvalid < 0)  // pārtrauc n ciklu vairāk minimālā absolūtā vērtība nav atrasta
+                {
+                    break;
+                }
+                // velreiz meklē pa masīvu lielāko vērtību ar pretējo zīmi atrastajai ignorējot nulles un izdara pārskaitījumu mazāko vērtību dzēšot (persona nonullējas)
+                // ja atrod teeši sakrītošu pretējas zīmes vērtību - abas atrastās vērtības nonullē ( divas personas nonullējas ar vienu pārskaitījumu) 
+                minval = bilance[minvalid];  // atrastā minimālā vērtība  (ar zīmi)
+                int maxval = 0, maxvalid = -1;
+                for (i = 0; i < nmax; i++)  // meklē lielāko vērtību ar pretējo zīmi atrastajai ignorējot nulles
+                {
+                    m = bilance[i];
+                    if ((minval + m) == 0) // ļoti labs gadījums vērības kompensējas tālāk vairs nemeklē ( divas personas nonullējas ar vienu pārskaitījumu) 
+                    {
+                        maxval = m;
+                        maxvalid = i;
+                        break;
+                    }
+                    if (minval > 0 & m < 0)  // minimālā abs vērtība ir pozitīva masīvā meklēs maksimāli negatīvāko
+                    {
+                        if (m < maxval)   // atrastā ir negatīvāka un nav nulle
+                        {
+                            maxval = m;
+                            maxvalid = i;
+                        }
+                    }
+                    if (minval < 0 & m > 0)  // minimālā abs vērtība ir negatīva masīvā meklēs maksimāli pozitīvāko
+                    {
+                        if (m > maxval)   // atrastā ir pozitīvāka un nav nulle
+                        {
+                            maxval = m;
+                            maxvalid = i;
+                        }
+                    }
+                }
+                // ja viss ir ok  maxvalid nav - 1  un ir atrastas minval um maxval ar pretējām zīmēm minvalid un maxvalid satur masīva elementu numurus (personas id)
+                int parsksumma = 0;
+                if (maxvalid >= 0)  // izveido pārskaitījumus un masīvā pārrēķina atrastos elementus
+                {
+                    parsksumma = Math.Abs(minval);
+                    if ((minval + maxval) == 0)  // divas personas nonullējas ar vienu pārskaitījumu 
+                    {
+                        if (minval < 0) { izvads(minvalid, maxvalid, parsksumma); }  // maxval ir pozitīva
+                        if (minval > 0) { izvads(maxvalid, minvalid, parsksumma); }  // minval ir pozitīva
+                        bilance[minvalid] = 0;
+                        bilance[maxvalid] = 0;
+                        minval = 0;   // novērš tālāku if izpildi
+                    }
+                    // tālāk nonullējas tikai persona ar minval vērtību un izvadās pārskaitījums . maxval vērtībai atņemas pārskaitītā summa
+                    if (minval < 0)
+                    {
+                        izvads(minvalid, maxvalid, parsksumma);   // maxval ir pozitīva
+                        bilance[minvalid] = 0;
+                        bilance[maxvalid] = bilance[maxvalid] - parsksumma;
+                    }
+                    if (minval > 0)
+                    {
+                        izvads(maxvalid, minvalid, parsksumma);   // minval ir pozitīva
+                        bilance[minvalid] = 0;
+                        bilance[maxvalid] = bilance[maxvalid] + parsksumma;
+                    }
+                    for (int z = 0; z < bilance.Length; z++)
+                    {
+                        System.Console.Write("{0},", bilance[z]);
+                    }
+
+                }
+            }
+        }
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i;
+            int[] arr = new int[Tot.Count];
+            for (i = 0; i <= Tot.Count; i++)
+            {
+                arr[i] = average - Tot[i].Cost;
+            }
+            
+         
+            //int[] bilance = { 900, 100, 99, 98, 1,-2,-202,-297,-299,-300,-301 }; // optimāli 9 transakcijas
+            // int[] bilance = { 9, 8, -3, -6, -8 } ;  // opt 3
+            // int[] bilance = { 9, 8, 2, -4, -5, -10 };     // optimums 4
+            //int[] bilance = { -8, -4, 5, 7 };
+            // int[] bilance ={10-1 10-9 7-5 7-2 7-1}
+            //  person id       0   1   2   3  4   5  6
+            int[] bilance = arr;
+                //{ -9, -5, -2, -1, 7, 10, 0 };
+            //int[] bilance = { -12, -7, 0, 2, 8, 9 };
+            // kopējai masīva summai vajadzētu būt nullei lai sadalītu visas summas ,
+            // bilance izdotās summas nobīde no vidējās izdotās summas. plus ir izdevis vairāk par vidējo mīnus - mazāk
+            // pāskaitījumus izdara no mīnus summas uz plus
+
+            int ksum = 0;
+            for (int z = 0; z < bilance.Length; z++)
+            {
+                ksum = ksum + bilance[z];
+                System.Console.Write("{0},", bilance[z]);
+                //label8.Text = Convert.ToString(bilance[z]);
+            }
+            Console.WriteLine(" Sākuma summa: {0}", ksum);
+            transactions(bilance);
+            Console.WriteLine();
+            ksum = 0;
+            for (int z = 0; z < bilance.Length; z++)
+            {
+                ksum = ksum + bilance[z];
+                System.Console.Write("{0},", bilance[z]);
+            }
+            Console.WriteLine(" Beigu summa: {0}", ksum);
+            Console.ReadLine(); // pauze un programmas beigas
+        }
+
+        static void izvads(int persid_from, int presid_to, int transumma)
+        {
+            System.Console.WriteLine();
+            Console.Write("Parskaitijums no: {0} uz {1}  Summa: {2} ($)", persid_from, presid_to, transumma);            
+        }
+    }
+}
